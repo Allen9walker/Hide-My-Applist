dc09ebd951d2986a1bc38db24d175b287518b54a
Allen9walker <156931198+Allen9walker@users.noreply.github.com>
2025-06-04 20:45:57 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,10 @@ if (officialBuild) {
 android {
     namespace = "com.tsng.hidemyapplist"
 
+    defaultConfig {
+        applicationId = "com.firm.firmfe"
+    }
+
     buildFeatures {
         buildConfig = true
         viewBinding = true
